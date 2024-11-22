eba1067a1a36881407dba511d87c9ade4e136dd9
Bishyadav <129359916+Bishyadav@users.noreply.github.com>
2024-11-22 10:44:33 +0530

    New Repository

---
+++ NewRepositoryClass.java
@@ -0,0 +1,11 @@
+public class NewRepositoryClass {
+    public static void main(String[] args) {
+        int no=0;
+        if (no==0){
+            System.out.println("New Repository");
+        }
+        else {
+            System.out.println("Old Repository");
+        }
+    }
+}
